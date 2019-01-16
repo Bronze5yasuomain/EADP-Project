25b778401fdce567eb9dc18c59606a4a57cdf63c
Shizuko <32918698+AhmadShafie@users.noreply.github.com>
2019-01-16 14:56:16 +0800

    Update TripDetails.aspx.cs

--- Website/TripDetails.aspx.cs
+++ Website/TripDetails.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +12,20 @@ public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+        using (SqlConnection sqlCon = new SqlConnection(DBConnect))
+        {
+            sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("Select * from Trip", sqlCon);
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            GridView1.DataSource = dtbl;
+            GridView1.DataBind();
+        }
+    }
 
+    protected void lnkSelect_Click(object sender, EventArgs e)
+    {
+        int tripid = Convert.ToInt32((sender as LinkButton).CommandArgument);
     }
 }
