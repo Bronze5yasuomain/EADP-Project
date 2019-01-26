a794507a708f605a082abfc9a7d3ea4e39ade741
Shizuko <32918698+AhmadShafie@users.noreply.github.com>
2019-01-26 09:18:33 +0800

    Add files via upload

---
+++ Website/PhotoGallery.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="PhotoGallery.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    <asp:FileUpload ID="FileUpload1" runat="server" />
+    <br /><br />
+    <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
+    <br /><br />
+    <asp:Label ID="lblMessage" runat="server"></asp:Label>
+    <br /><br />
+    <asp:HyperLink ID="hyperlink" runat="server">View Uploaded Image</asp:HyperLink>
+    </div>
+         <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="picId" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" OnRowCommand="GridView1_RowCommand">
+    <Columns>
+        <asp:BoundField DataField="picName" HeaderText="Name" />
+        <asp:BoundField DataField="picStatus" />
+        <asp:TemplateField HeaderText="Image">
+            <ItemTemplate>
+                <asp:Image ID="Image1" runat="server" Height="100px" Width="100px"
+                    ImageUrl='<%# "data:Image/png;base64,"
+                    + Convert.ToBase64String((byte[])Eval("ImageData")) %>' />
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField>
+    <ItemTemplate>
+        <asp:Button ID="btnpreview" runat="server" Text="Remove" CommandName="Remove" />
+    </ItemTemplate>
+</asp:TemplateField>
+        <asp:CommandField ShowSelectButton="True" SelectText="Approve" />
+    </Columns>
+</asp:GridView>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <asp:Panel ID="PanelPictures" runat="server" BorderColor="#000066" BorderStyle="Solid" HorizontalAlign="Right" BackColor="#FF5050">
+                                <asp:DataList id="Images" 
+                                           RepeatDirection="Horizontal"
+                                           RepeatLayout="Table"
+                                           RepeatColumns="0" runat="server">
+                                         <HeaderStyle BackColor="#CC0066">
+                                         </HeaderStyle>
+                                         <AlternatingItemStyle BackColor="#FF5050">
+                                         </AlternatingItemStyle>
+                                         <HeaderTemplate>
+                                            Approved Images
+                                         </HeaderTemplate>
+                                         <ItemTemplate>
+                                            <asp:Image id="ProductImage" runat="server" Height="150px" Width="150px" ImageUrl='<%#"data:Image/png;base64," + Convert.ToBase64String((byte[])Eval("Imagedata")) %>' CssClass="image" />
+                                         </ItemTemplate>
+                                    </asp:DataList>
+                            </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Website/PhotoGallery.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            lblMessage.Visible = false;
+            hyperlink.Visible = false;
+            LoadImages();
+        }
+    }
+
+    private void LoadImages()
+    {
+        string cs = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            SqlCommand cmd = new SqlCommand("Select * from tblImages where picStatus = 'Pending'", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            rdr.Close();
+            SqlCommand images = new SqlCommand("Select * from tblImages where picStatus = 'Approved'", con);
+            SqlDataReader rdr1 = images.ExecuteReader();
+            Images.DataSource = rdr1;
+            Images.DataBind();
+            con.Close();
+        }
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        HttpPostedFile postedFile = FileUpload1.PostedFile;
+        string filename = Path.GetFileName(postedFile.FileName);
+        string fileExtension = Path.GetExtension(filename);
+        int fileSize = postedFile.ContentLength;
+
+        if (fileExtension.ToLower() == ".jpg" || fileExtension.ToLower() == ".gif"
+            || fileExtension.ToLower() == ".png" || fileExtension.ToLower() == ".bmp")
+        {
+            Stream stream = postedFile.InputStream;
+            BinaryReader binaryReader = new BinaryReader(stream);
+            Byte[] bytes = binaryReader.ReadBytes((int)stream.Length);
+
+
+            string cs = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spUploadImage", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramName = new SqlParameter()
+                {
+                    ParameterName = @"picName",
+                    Value = filename
+                };
+                cmd.Parameters.Add(paramName);
+
+                SqlParameter paramSize = new SqlParameter()
+                {
+                    ParameterName = "@picSize",
+                    Value = fileSize
+                };
+                cmd.Parameters.Add(paramSize);
+
+                SqlParameter paramImageData = new SqlParameter()
+                {
+                    ParameterName = "@ImageData",
+                    Value = bytes
+                };
+                cmd.Parameters.Add(paramImageData);
+
+                SqlParameter parampicStatus = new SqlParameter()
+                {
+                    ParameterName = "@picStatus",
+                    Value = "Pending"
+                };
+                cmd.Parameters.Add(parampicStatus);
+
+                SqlParameter paramNewId = new SqlParameter()
+                {
+                    ParameterName = "@NewId",
+                    Value = -1,
+                    Direction = ParameterDirection.Output
+                };
+                cmd.Parameters.Add(paramNewId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                lblMessage.Visible = true;
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                lblMessage.Text = "Upload Successful";
+                hyperlink.Visible = true;
+                hyperlink.NavigateUrl = "~/WebForm2.aspx?Id=" +
+                    cmd.Parameters["@NewId"].Value.ToString();
+            }
+        }
+        else
+        {
+            lblMessage.Visible = true;
+            lblMessage.ForeColor = System.Drawing.Color.Red;
+            lblMessage.Text = "Only images (.jpg, .png, .gif and .bmp) can be uploaded";
+            hyperlink.Visible = false;
+        }
+    }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int rowindex = int.Parse(GridView1.SelectedValue.ToString());
+        Label1.Text = rowindex.ToString();
+        string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+        SqlConnection con = new SqlConnection(DBConnect);
+        SqlCommand approve = new SqlCommand("update tblImages set picStatus = 'Approved' where picId = @parapicId", con);
+        approve.Parameters.AddWithValue("@parapicId", rowindex);
+        con.Open();
+        approve.ExecuteNonQuery();
+        con.Close();
+        Label1.Text = rowindex.ToString() + " approved successfully";
+        LoadImages();
+    }
+
+    protected void GridView1_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "Remove")
+        {
+            GridViewRow row = (GridViewRow)(((Button)e.CommandSource).NamingContainer);
+            int id = (int)GridView1.DataKeys[row.RowIndex].Value;
+            Label1.Text = id.ToString();
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection con = new SqlConnection(DBConnect);
+            SqlCommand delete = new SqlCommand("delete from tblImages where picId = @parapicId ", con);
+            delete.Parameters.AddWithValue("@parapicId", id);
+
+            con.Open();
+            delete.ExecuteNonQuery();
+            con.Close();
+            Label1.Text = id.ToString() + " deleted successfully";
+            LoadImages();
+        }
+    }
+}
